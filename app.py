86bc810ab01d56a6cbe259efed18610771d7f2c5
jamineee <98871254+jamineee@users.noreply.github.com>
2026-03-11 12:19:56 +0900

    Fix text drawing parameters in app.py

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ def make_slide(text):
     img = Image.new("RGB",(WIDTH,HEIGHT),"white")
     draw = ImageDraw.Draw(img)
 
-    draw.text((WIDTH/2,HEIGHT/2), word, font==font_en, fill="black",anchor="mm")
-    draw.text((WIDTH/2,HEIGHT/2), ko_meaning, font==font_ko, fill="black",anchor="mm")
-    draw.text(((WIDTH/2,HEIGHT/2), en_meaning, font==font_en, fill="black",anchor="mm")
-    draw.text((WIDTH/2,HEIGHT/2), example, font==font_en, fill="black",anchor="mm")
-    draw.text((WIDTH/2,HEIGHT/2), example_ko, font==font_ko, fill="black",anchor="mm")
+    draw.text((WIDTH/2,HEIGHT/2), word, font==font_en, fill=="black",anchor=="mm")
+    draw.text((WIDTH/2,HEIGHT/2), ko_meaning, font==font_ko, fill=="black",anchor=="mm")
+    draw.text(((WIDTH/2,HEIGHT/2), en_meaning, font==font_en, fill=="black",anchor=="mm")
+    draw.text((WIDTH/2,HEIGHT/2), example, font==font_en, fill=="black",anchor=="mm")
+    draw.text((WIDTH/2,HEIGHT/2), example_ko, font==font_ko, fill=="black",anchor=="mm")
 
     tmp = tempfile.NamedTemporaryFile(delete=False,suffix=".png")
     img.save(tmp.name)
